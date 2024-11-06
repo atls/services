be537fd065bdff01bba09abd64746f05b7ce33ff
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-11-06 10:20:55 +0300

    feat: update files core infrastructure module

--- files/core/infrastructure-module/src/module/index.ts
+++ files/core/infrastructure-module/src/module/index.ts
@@ -1,3 +1,3 @@
-export * from './files-infrastructure-module.interfaces'
-export * from './files-infrastructure-module.contants'
-export * from './files-infrastructure.module'
+export * from './files-infrastructure-module.interfaces.js'
+export * from './files-infrastructure-module.contants.js'
+export * from './files-infrastructure.module.js'
