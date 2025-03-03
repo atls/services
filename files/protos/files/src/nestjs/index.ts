31b55983fc1991c87654abe3050686d746cd71ac
Andrew Ghostuhin <me@torinasakura.name>
2025-03-03 04:24:00 +0300

    chore(files-rpc): add buf repo to deps

--- files/protos/files/src/nestjs/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './files-service-client.module.js'
-export * from './gateway-handler.options.js'
--- files/protos/files/src/path.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types='@monstrs/types-import-proto'/>
-
-import * as path from 'node:path'
-
-const dirname = path.dirname(new URL(import.meta.url).pathname)
-
-export const filesServicePath = path.join(dirname, '../tech/atls/files/v1/files.service.proto')
-
-export const includeDirs = [dirname, path.join(dirname, '..')]
