f4373cee207ee31e48147ce31fd9df8d4844f121
Dmitry <dimafomin02@gmail.com>
2025-03-18 00:11:52 +0300

    chore(files-rpc): trigger npm publish

--- files/rpc/files-rpc-client-module/src/index.ts
+++ files/rpc/files-rpc-client-module/src/index.ts
@@ -1,5 +1,5 @@
-export * from '@atls/files-rpc/connect'
 export * from '@bufbuild/protobuf'
+export * from '@atls/files-rpc/connect'
 
 export * from './client/index.js'
 export * from './module/index.js'
