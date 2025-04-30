49a561c780fc9c89b808b4b28bdf07b5a725cb13
Dmitry <dimafomin02@gmail.com>
2025-05-01 02:20:39 +0300

    feat(files): add health resolver

--- files/service/gateway-module/src/module/files-gateway.module.ts
+++ files/service/gateway-module/src/module/files-gateway.module.ts
@@ -4,13 +4,21 @@ import { Module }             from '@nestjs/common'
 
 import * as dataLoaders       from '../data-loaders/index.js'
 import * as mutations         from '../mutations/index.js'
+import * as queries           from '../queries/index.js'
 
 @Module({})
 export class FilesGatewayModule {
   static register(): DynamicModule {
+    const providers = [
+      ...Object.values(mutations),
+      ...Object.values(queries),
+      ...Object.values(dataLoaders),
+    ]
+
     return {
       module: FilesGatewayModule,
-      providers: [...Object.values(mutations), ...Object.values(dataLoaders)],
+      providers,
+      exports: providers,
     }
   }
 }
