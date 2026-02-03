dbac5bc6a7a5ebab4cedb937926500f2b7e2562f
Dmitry Fomin <80476878+Amiditin@users.noreply.github.com>
2026-02-03 19:56:24 +0300

    fix(auth): rpc packages naming (#50)

--- auth/rpc/auth/src/gen/connect/index.ts
+++ auth/rpc/auth/src/gen/connect/index.ts
@@ -1,2 +1,2 @@
-export * from './team/firstfood/auth/v1/auth.payloads_pb.js'
-export * from './team/firstfood/auth/v1/auth.service_connect.js'
+export * from './tech/atls/auth/v1/auth.payloads_pb.js'
+export * from './tech/atls/auth/v1/auth.service_connect.js'
