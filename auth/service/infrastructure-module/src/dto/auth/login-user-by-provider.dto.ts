ec19e4bd717d84482f2732558a81241ab889e96d
Dmitry Fomin <dimafomin02@gmail.com>
2026-02-03 05:38:50 +0300

    fix(common): packages

--- auth/service/infrastructure-module/src/dto/auth/login-user-by-provider.dto.ts
+++ auth/service/infrastructure-module/src/dto/auth/login-user-by-provider.dto.ts
@@ -2,8 +2,8 @@ import { IsEnum }                          from 'class-validator'
 import { IsNotEmpty }                      from 'class-validator'
 import { IsString }                        from 'class-validator'
 
-import { ProviderType as RpcProviderType } from '@alts/auth-rpc/abstractions'
-import { ProviderType as ProviderTypeDto } from '@alts/auth-rpc/interfaces'
+import { ProviderType as RpcProviderType } from '@atls/auth-rpc/abstractions'
+import { ProviderType as ProviderTypeDto } from '@atls/auth-rpc/interfaces'
 
 export class LoginUserByProviderDto {
   @IsEnum(RpcProviderType)
